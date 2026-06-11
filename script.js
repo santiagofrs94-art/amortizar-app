bc37778194f80bfc20dc9a582188e71a7f3ed51a
santiagofrs94-art <santiagofrs94@gmail.com>
2026-06-10 23:22:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
-function salvarGasto() {
-  alert("Conexão com Supabase será feita no próximo passo.");
+let saldo = Number(localStorage.getItem("saldo")) || 0;
+let totalAmortizado = Number(localStorage.getItem("totalAmortizado")) || 0;
+
+atualizarTela();
+
+function amortizar() {
+
+    const saldoInformado =
+        Number(document.getElementById("saldo").value);
+
+    const valorAmortizacao =
+        Number(document.getElementById("amortizacao").value);
+
+    if (saldo === 0 && saldoInformado > 0) {
+        saldo = saldoInformado;
+    }
+
+    saldo -= valorAmortizacao;
+
+    totalAmortizado += valorAmortizacao;
+
+    localStorage.setItem("saldo", saldo);
+    localStorage.setItem("totalAmortizado", totalAmortizado);
+
+    atualizarTela();
+
+    document.getElementById("amortizacao").value = "";
+}
+
+function atualizarTela() {
+
+    document.getElementById("saldoAtual").innerHTML =
+        saldo.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        });
+
+    document.getElementById("totalAmortizado").innerHTML =
+        totalAmortizado.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        });
 }
